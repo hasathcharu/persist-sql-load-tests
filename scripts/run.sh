a0cf5a9eacab7840d652822dbdd1daee85c3ce89
Haritha Hasathcharu <42366642+hasathcharu@users.noreply.github.com>
2024-05-20 12:11:22 +0530

    Minor changes to script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh -e
 set -e
-/home/haritha/Downloads/apache-jmeter-5.6.3/bin/jmeter -n -t ./Test\ Plan.jmx -Jtype="$TEST_TYPE"
+jmeter -n -t ./Test\ Plan.jmx -Jtype="$TEST_TYPE" -Jhost=13.126.1.187

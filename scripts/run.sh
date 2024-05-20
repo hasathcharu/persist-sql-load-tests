492c53c62655850a797f7ff8d6bb2ec1c2806820
Haritha Hasathcharu <42366642+hasathcharu@users.noreply.github.com>
2024-05-20 19:13:46 +0530

    Optimize jmeter script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 set -e
-jmeter -n -t ./Test\ Plan.jmx -Jtype="$TEST_TYPE" -Jhost=13.126.1.187
+jmeter -n -t ./Test\ Plan.jmx -l row_results/"$TEST_TYPE"/Pass"$PASS".jtl -Jtype="$TEST_TYPE" -Jhost=13.126.1.187
